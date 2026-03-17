fc87ca0c5425ab499c526f0e0fc99697fa788d06
LanzVi <villaraza.lanzaderas21@gmail.com>
2026-03-17 16:43:58 +0800

    Code Complete. Code Arrangement on Index.js complete. programs works as intended.

--- index.js
+++ index.js
@@ -2,6 +2,28 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//middleware to parse JSON bodies
+app.use(express.json());
+
+const items = ['Apple', 'Banana', 'Orange'];
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
+app.get('/items', (req, res) => {
+    res.json(items);
+});
+
+app.post('/items', (req, res) => {
+    const newItem = req.body.item;
+    if (newItem) {
+        items.push(newItem);
+        res.json(items);
+    }
+});
+
 app.use(express.static('public'));
 
 //route for Home page
@@ -9,6 +31,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+//Start the server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
@@ -17,11 +40,14 @@ app.get('/about', (req, res) => {
     res.send('About Us');
 });
 
-//middleware to parse JSON bodies
-app.use(express.json());
-
 app.post('/submit', (req, res) => {
     const data = req.body;
-    console.log('Received data:', data);
+    //console.log('Received data:', data);
     res.send(`Received: ${JSON.stringify(data)}`);
 });
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
